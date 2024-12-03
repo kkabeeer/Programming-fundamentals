ad554fd0f066f3a3bd52e17eedf77194a72dd7eb
kabeeer <kabeerrabwanikabeer@gmail.com>
2024-12-03 20:30:46 +0500

    Update Positive numbers.cpp

--- Positive numbers.cpp
+++ Positive numbers.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-
 int main()
 {
-	int num=-4;
-	if(num>0){
-	
-	printf("This is a positive number");
-	}if(num<0){
-		printf("This is a negative number");
-		
-	}
-	return 0;
+    int num = -4;
+    if (num > 0) {
+        std::cout << "This is a positive number" << std::endl;
+    } else if (num < 0) {
+        std::cout << "This is a negative number" << std::endl;
+    } else {
+        std::cout << "This is zero" << std::endl;
+    }
+
+    return 0;
 }
